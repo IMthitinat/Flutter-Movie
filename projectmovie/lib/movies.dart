acdca01d6a09650a9f3f7a7554d7dd16a3af4336
IMthitinat <IMthitinat@users.noreply.github.com>
2022-02-01 21:03:54 +0700

    coming section
    picture discription

--- projectmovie/lib/movies.dart
+++ projectmovie/lib/movies.dart
@@ -198,6 +198,214 @@ class MoviesPage extends StatelessWidget {
                     )),
               ],
             ),
+            SizedBox(
+              height: 15,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Coming Soon',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'Light',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 22,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        children: [
+                          Container(
+                            alignment: Alignment.bottomCenter,
+                            height: 160.0,
+                            width: 130.0,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              image: DecorationImage(
+                                image: AssetImage('assets/images/movie4.jpg'),
+                                fit: BoxFit.fill,
+                              ),
+                              boxShadow: [
+                                //background color of box
+                                BoxShadow(
+                                  color: Colors.black,
+                                  blurRadius: 3.0, // soften the shadow
+                                  spreadRadius: 2.0, //extend the shadow
+                                  offset: Offset(
+                                    3.0, // Move to right 10  horizontally
+                                    3.0, // Move to bottom 10 Vertically
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          SizedBox(width: 20),
+                          Container(
+                            alignment: Alignment.bottomCenter,
+                            height: 160.0,
+                            width: 130.0,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              image: DecorationImage(
+                                image: AssetImage('assets/images/movie7.jpg'),
+                                fit: BoxFit.fill,
+                              ),
+                              boxShadow: [
+                                //background color of box
+                                BoxShadow(
+                                  color: Colors.black,
+                                  blurRadius: 3.0, // soften the shadow
+                                  spreadRadius: 2.0, //extend the shadow
+                                  offset: Offset(
+                                    3.0, // Move to right 10  horizontally
+                                    3.0, // Move to bottom 10 Vertically
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          SizedBox(width: 20),
+                          Container(
+                            alignment: Alignment.bottomCenter,
+                            height: 160.0,
+                            width: 130.0,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              image: DecorationImage(
+                                image: AssetImage('assets/images/movie1.jpg'),
+                                fit: BoxFit.fill,
+                              ),
+                              boxShadow: [
+                                //background color of box
+                                BoxShadow(
+                                  color: Colors.black,
+                                  blurRadius: 3.0, // soften the shadow
+                                  spreadRadius: 2.0, //extend the shadow
+                                  offset: Offset(
+                                    3.0, // Move to right 10  horizontally
+                                    3.0, // Move to bottom 10 Vertically
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 20),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          SizedBox(width: 30),
+                          Text(
+                            " MY HERO ACADEMIA",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Light',
+                              fontSize: 10,
+                            ),
+                          ),
+                          SizedBox(width: 55),
+                          Text(
+                            " QUITE PLACE",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Light',
+                              fontSize: 10,
+                            ),
+                          ),
+                          SizedBox(width: 100),
+                          Text(
+                            " POP-EYE",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Light',
+                              fontSize: 10,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: <Widget>[
+                          SizedBox(width: 50),
+                          Icon(
+                            Icons.star,
+                            size: 15,
+                            color: Colors.yellow,
+                          ),
+                          Icon(
+                            Icons.star,
+                            size: 15,
+                            color: Colors.yellow,
+                          ),
+                          Icon(
+                            Icons.star,
+                            size: 15,
+                            color: Colors.yellow,
+                          ),
+                          Text("Inter vote",
+                              style: TextStyle(
+                                fontSize: 8,
+                                color: Colors.white,
+                              )),
+                          SizedBox(width: 50),
+                          Icon(
+                            Icons.star,
+                            size: 15,
+                            color: Colors.yellow,
+                          ),
+                          Icon(
+                            Icons.star,
+                            size: 15,
+                            color: Colors.yellow,
+                          ),
+                          Icon(
+                            Icons.star,
+                            size: 15,
+                            color: Colors.yellow,
+                          ),
+                          Text("Inter vote",
+                              style: TextStyle(
+                                fontSize: 8,
+                                color: Colors.white,
+                              )),
+                          SizedBox(width: 70),
+                          Icon(
+                            Icons.star,
+                            size: 15,
+                            color: Colors.yellow,
+                          ),
+                          Icon(
+                            Icons.star,
+                            size: 15,
+                            color: Colors.yellow,
+                          ),
+                          Icon(
+                            Icons.star,
+                            size: 15,
+                            color: Colors.yellow,
+                          ),
+                          Text("Inter vote",
+                              style: TextStyle(
+                                fontSize: 8,
+                                color: Colors.white,
+                              )),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
