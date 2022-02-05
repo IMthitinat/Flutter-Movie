1844a631571243f870c1bbb224726e14ff0ea507
chatlada07 <86917541+chatlada07@users.noreply.github.com>
2022-02-05 15:51:17 +0700

    Creted Spiderman details page and Creted StatefulWidget

---
+++ projectmovie/lib/SpiderManDetailsPage.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: prefer_const_constructors, unnecessary_new
+
+import 'package:flutter/material.dart';
+import 'package:smooth_star_rating/smooth_star_rating.dart';
+
+class SpiderManDetail extends StatefulWidget {
+  const SpiderManDetail({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+  @override
+  State<SpiderManDetail> createState() => _SpiderManDetailState();
+}
